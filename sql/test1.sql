826c196145b181ed1a4ee7965438744e971b2cc0
ouchin55edcx <ouchinmustapha82@gmail.com>
2023-11-30 10:23:15 +0100

    .

--- sql/test1.sql
+++ sql/test1.sql
@@ -9,14 +9,14 @@ CREATE TABLE `cart` (
   `product_id` int(11) DEFAULT NULL,
   `quantity` int(11) DEFAULT 1,
   `userId` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+);
 
 
 CREATE TABLE `categorie` (
   `id` int(11) NOT NULL,
   `ctg_name` varchar(100) DEFAULT NULL,
   `ctg_img_path` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+);
 
 
 
@@ -25,7 +25,7 @@ CREATE TABLE `comments` (
   `cart_id` int(11) DEFAULT NULL,
   `comment_text` text DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+);
 
 
 
@@ -36,14 +36,14 @@ CREATE TABLE `plante` (
   `prix` int(11) DEFAULT NULL,
   `image` varchar(255) DEFAULT NULL,
   `categorieID` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+);
 
 
 
 CREATE TABLE `roles` (
   `id` int(11) NOT NULL,
   `name` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+);
 
 
 INSERT INTO `roles` (`id`, `name`) VALUES
@@ -58,58 +58,6 @@ CREATE TABLE `users` (
   `firstName` varchar(255) NOT NULL,
   `lastName` varchar(255) NOT NULL,
   `role_id` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
-
-ALTER TABLE `cart`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `product_id` (`product_id`),
-  ADD KEY `fk_userId` (`userId`);
-
-
-ALTER TABLE `categorie`
-  ADD PRIMARY KEY (`id`);
-
-ALTER TABLE `comments`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `cart_id` (`cart_id`);
-
-
-ALTER TABLE `plante`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `categorieID` (`categorieID`);
-
-ALTER TABLE `roles`
-  ADD PRIMARY KEY (`id`);
-
-
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `email` (`email`),
-  ADD KEY `role_id` (`role_id`);
-
-
-ALTER TABLE `cart`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
-
-ALTER TABLE `categorie`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=99;
-
-
-ALTER TABLE `comments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
-
-ALTER TABLE `plante`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
-
-
-ALTER TABLE `roles`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
-
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
-
+) ;
 
 
