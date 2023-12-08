16c3c50e222e2da05a99cb7bd40576b007ce35cc
ouchin55edcx <ouchinmustapha82@gmail.com>
2023-12-08 16:38:21 +0100

    #

--- client/header.php
+++ client/header.php
@@ -43,20 +43,6 @@
                     </div>
                 </div>
 
-                <div class='max-w-md mx-auto'>
-                    <div class="relative flex items-center w-full h-12 rounded-lg focus-within:shadow-lg bg-white overflow-hidden">
-                        <form action="" method="post">
-                            <input class="peer h-full w-full outline-none text-sm text-gray-700 pr-2" type="text" name="search" placeholder="Search something.." />
-                            <button type="submit">
-                                <div class="grid place-items-center h-full w-12 text-gray-300">
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-                                    </svg>
-                                </div>
-                            </button>
-                        </form>
-                    </div>
-                </div>
 
                 <div class="ml-auto flex items-center">
                     <div class="hidden lg:flex lg:flex-1 lg:items-center lg:justify-end lg:space-x-6">
--- client/product.php
+++ client/product.php
@@ -3,6 +3,20 @@ include("../admin/head.php");
 include("../admin/connect.php");
 include("header.php");
 ?>
+<div class='max-w-md mx-auto'>
+    <div class="relative flex items-center w-full h-12 rounded-lg focus-within:shadow-lg bg-white overflow-hidden">
+        <form action="" method="post">
+            <input class="peer h-full w-full outline-none text-sm text-gray-700 pr-2" type="text" name="search" placeholder="Search something.." />
+            <button type="submit">
+                <div class="grid place-items-center h-full w-12 text-gray-300">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+                    </svg>
+                </div>
+            </button>
+        </form>
+    </div>
+</div>
 
 <section>
 
@@ -15,21 +29,20 @@ include("header.php");
 
 </section>
 
-    
+
 <?php
-if($_SERVER['REQUEST_METHOD'] == 'POST'){
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     //searsh
     $search = $_POST['search'];
     $query = "SELECT * FROM products  WHERE product_name LIKE '%$search%'";
-    $result = mysqli_query($con, $query); 
-
-} else{
+    $result = mysqli_query($con, $query);
+} else {
     $result = mysqli_query($con, "SELECT * FROM products");
 }
 
 ?>
- 
- <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 p-6">
+
+<div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 p-6">
     <?php
     while ($row = mysqli_fetch_array($result)) {
         echo "
@@ -53,12 +66,6 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
     
         </main>
     </center> ";
-   }
+    }
     ?>
 </div>
-
-
-
-
-
-
